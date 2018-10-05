c4c7e00cab85a2b0f6e0fad5e16b842c2891dacf
Morten Ottersen <mo@eiendomsverdi.no>
2018-10-05 10:02:19 +0200

    testing viewport2

--- wwwroot/viewporttester/styles.js
+++ wwwroot/viewporttester/styles.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "/* You can add global styles to this file, and also import other style files */\nbody, html {\n    height: 100vh;\n    margin: 0;\n}\nhtml{\n    overflow: hidden;\n    width:100%;\n}\nbody{\n    height:100%;\n    position:fixed; /* prevent overscroll bounce*/\n    overflow-y:scroll;\n    -webkit-overflow-scrolling: touch; /* iOS velocity scrolling */\n    width:50%;                    \n    margin-left:25%;\n} "
+module.exports = "/* You can add global styles to this file, and also import other style files */\nbody, html {\n    height: 100vh;\n    margin: 0;\n}\nhtml{\n    overflow: hidden;\n    width:100%;\n}\nbody{\n    position: absolute;\n    background-color: aquamarine;\n    top: 0;\n    left: 0;\n    right: 0;\n    bottom: 0;\n    overflow: scroll;\n    -webkit-overflow-scrolling: touch;\n    overflow-scrolling: touch;\n} "
 
 /***/ }),
 
