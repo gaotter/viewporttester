853c9e938460287f81dc6ef8ba1289d31cdec62b
Morten Ottersen <mo@eiendomsverdi.no>
2018-10-05 10:14:41 +0200

    testing viewport3

--- wwwroot/viewporttester/styles.js
+++ wwwroot/viewporttester/styles.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "/* You can add global styles to this file, and also import other style files */\nhtml, body {\n    height: 100%;\n    overflow: hidden;\n}\nbody{\n    position: absolute;\n    background-color:rebeccapurple;\n    top: 0;\n    left: 0;\n    right: 0;\n    bottom: 0;\n    overflow: scroll;\n    -webkit-overflow-scrolling: touch;\n    overflow-scrolling: touch;\n} "
+module.exports = "/* You can add global styles to this file, and also import other style files */\nhtml, body {\n    height: 100%;\n    overflow: hidden;\n}\nbody, #page{\n    position: absolute;\n    background-color:rebeccapurple;\n    top: 0;\n    left: 0;\n    right: 0;\n    bottom: 0;\n    overflow: scroll;\n    -webkit-overflow-scrolling: touch;\n    overflow-scrolling: touch;\n} "
 
 /***/ }),
 
